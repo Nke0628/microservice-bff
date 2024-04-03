3bf0b0fdf4e217f187e12bbc66776cda2333455d
Nke0628 <ooo_diva@yahoo.co.jp>
2024-04-03 21:35:00 +0900

    #4 feat:graphQLにおけるenum調整

--- src/module/user/infrastructure/user.repository.mock.ts
+++ src/module/user/infrastructure/user.repository.mock.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 import { IUserRepository } from './user.repository.interface';
-import { UserBase } from '../model/user.model';
+import { UserBase, UserType } from '../model/user.model';
 
 @Injectable()
 export class UserRepositoryMock implements IUserRepository {
@@ -11,11 +11,13 @@ export class UserRepositoryMock implements IUserRepository {
         id: i + 1,
         name: faker.internet.userName(),
         departmentId: 'S' + (i + 1),
+        userType: UserType.EMPLOYEE,
       }));
       mockData.push({
         id: 101,
         name: faker.internet.userName(),
         departmentId: '',
+        userType: UserType.EMPLOYEE,
       });
       const filteredMockData = mockData.filter((user) =>
         userIds.includes(user.id),
